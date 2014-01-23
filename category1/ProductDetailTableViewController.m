d3416c58e35d4c28c0955b0283e75a15a5bd84f0
matthewappbox <matthewho@appbox.com.my>
2014-01-23 23:27:45 +0800

    Update asynctask image loader

--- category1/ProductDetailTableViewController.m
+++ category1/ProductDetailTableViewController.m
@@ -14,8 +14,8 @@
 @interface ProductDetailTableViewController ()
 @property (strong,nonatomic) NSDictionary *productBasic;
 @property (strong,nonatomic) NSDictionary *productStock;
-@property (strong,nonatomic) NSDictionary *productField;
-@property (strong,nonatomic) NSDictionary *productImages;
+@property (strong,nonatomic) NSArray *productField;
+@property (strong,nonatomic) NSArray *productImages;
 @property (strong,nonatomic) PagedImageScrollView *pageScrollView;
 @property (nonatomic) NSUInteger totalImageCount;
 @property (strong,nonatomic) NSMutableArray *productImagesDataArray;
@@ -50,31 +50,31 @@
 #pragma mark - fetchData / Update data
    
 - (void)fetchProductDetail{
-        mainConfiguration *mC = [[mainConfiguration alloc] init];
-        dispatch_queue_t fetchCategoryQueue = dispatch_queue_create("fetchCategoryJSON", NULL);
-        dispatch_async(fetchCategoryQueue , ^{
-            NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://crmtest.appbox.com.my/products.apps.php"]]];
-            NSString *post = [NSString stringWithFormat:@"%@action=productdetail&productid=%@", [mC memberQueue], self.productID];
-            [request setValue:@"gzip" forHTTPHeaderField:@"Accept-Encoding"];
-            [request setHTTPBody:[post dataUsingEncoding:NSUTF8StringEncoding]];
-            [request setHTTPMethod:@"POST"];
-            NSError *error = nil;
-            NSURLResponse *response = nil;
-            NSData *contentInJSON = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-            NSDictionary *contentPropertyList = [NSJSONSerialization JSONObjectWithData:contentInJSON options:NSJSONReadingMutableContainers error:NULL];
-            dispatch_async(dispatch_get_main_queue(), ^(void){
-                if(![contentPropertyList valueForKeyPath:@"error"]){
-                    self.productBasic = [contentPropertyList valueForKeyPath:@"basic"];
-                    self.productStock = [contentPropertyList valueForKeyPath:@"stock"];
-                    self.productField = [contentPropertyList valueForKeyPath:@"field"];
-                    self.productImages = [contentPropertyList valueForKeyPath:@"images"];
-                    [self.tableView reloadData];
-                }
-            });
+    mainConfiguration *mC = [[mainConfiguration alloc] init];
+    dispatch_queue_t fetchCategoryQueue = dispatch_queue_create("fetchCategoryJSON", NULL);
+    dispatch_async(fetchCategoryQueue , ^{
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://crmtest.appbox.com.my/products.apps.php"]]];
+        NSString *post = [NSString stringWithFormat:@"%@action=productdetail&productid=%@", [mC memberQueue], self.productID];
+        [request setValue:@"gzip" forHTTPHeaderField:@"Accept-Encoding"];
+        [request setHTTPBody:[post dataUsingEncoding:NSUTF8StringEncoding]];
+        [request setHTTPMethod:@"POST"];
+        NSError *error = nil;
+        NSURLResponse *response = nil;
+        NSData *contentInJSON = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+        NSDictionary *contentPropertyList = [NSJSONSerialization JSONObjectWithData:contentInJSON options:NSJSONReadingMutableContainers error:NULL];
+        dispatch_async(dispatch_get_main_queue(), ^(void){
+            if(![contentPropertyList valueForKeyPath:@"error"]){
+                self.productBasic = [contentPropertyList valueForKeyPath:@"basic"];
+                self.productStock = [contentPropertyList valueForKeyPath:@"stock"];
+                self.productField = [contentPropertyList valueForKeyPath:@"field"];
+                self.productImages = [contentPropertyList valueForKeyPath:@"images"];
+                [self.tableView reloadData];
+            }
         });
-    }
+    });
+}
 
-# pragma mark - table configuration 
+# pragma mark - table configuration
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -100,7 +100,6 @@
 
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
-    self.tableView.hidden = TRUE;
 }
 
 #pragma mark - Table view data source
@@ -128,30 +127,25 @@
     if(indexPath.row == 0){
         static NSString *CellIdentifier = @"Product Image Cell";
         UITableViewCell *cell = (UITableViewCell *)[tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-        if((!self.pageScrollView)||(self.rotating)){
+        if(((!self.pageScrollView)||(self.rotating))&&([self.productImages count] > 0)){
             CGFloat currentpage = 0;
             if(self.pageScrollView){
                 if(self.pageScrollView.pageControl.currentPage)
                     currentpage = self.pageScrollView.pageControl.currentPage;
             }
-            self.pageScrollView = [[PagedImageScrollView alloc] initWithFrame:CGRectMake(0, 0, cell.frame.size.width, 180)];
             self.totalImageCount = [self.productImages count];
             for (NSDictionary *productImageDictionary in self.productImages) {
-                NSURL *thisImageUrl = [NSURL URLWithString:[productImageDictionary valueForKeyPath:@"url"]];
+                NSURL *thisImageUrl = [NSURL URLWithString:[productImageDictionary valueForKeyPath:@"imageurl"]];
                 dispatch_queue_t fetchCategoryQueue = dispatch_queue_create("fetchImage", NULL);
                 dispatch_async(fetchCategoryQueue , ^{
-                    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:thisImageUrl];
-                    [request setValue:@"gzip" forHTTPHeaderField:@"Accept-Encoding"];
-                    [request setHTTPMethod:@"GET"];
-                    NSError *error = nil;
-                    NSURLResponse *response = nil;
-                    NSData *responseImage = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-                    UIImage *thisReturnImage = [UIImage imageWithData:responseImage];
+                    UIImage *thisReturnImage = [UIImage imageWithData:[NSData dataWithContentsOfURL:thisImageUrl]];
                     dispatch_async(dispatch_get_main_queue(), ^(void){
                         [self updateImageSlider:thisReturnImage];
                     });
                 });
             }
+            //self.pageScrollView = [[PagedImageScrollView alloc] initWithFrame:CGRectMake(0, 0, cell.frame.size.width, 180)];
+            
             if((currentpage > 0) &&(self.rotating)){
                 [self.pageScrollView scrollToPreviousPage:currentpage];
             }
@@ -169,15 +163,20 @@
         cell.selectedBackgroundView.backgroundColor = [UIColor whiteColor];
         return cell;
     }
-    
-    
 }
 
 - (void)updateImageSlider:(UIImage *)thisImage{
+    if(!self.productImagesDataArray)
+        self.productImagesDataArray = [[NSMutableArray alloc] init];
     [self.productImagesDataArray addObject:thisImage];
     if([self.productImagesDataArray count] == self.totalImageCount){
+        self.pageScrollView = [[PagedImageScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 180)];
         [self.pageScrollView setScrollViewContents:self.productImagesDataArray];
+        [self.tableView reloadData];
     }
+    //self.pageScrollView = [[PagedImageScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 180)];
+    //[self.pageScrollView setScrollViewContents:@[[UIImage imageNamed:@"logo1"],[UIImage imageNamed:@"logo2"]]];
+    
 }
 
 #pragma mark - Navigation
